27b56a2d957f782ff05fa699a1b7a9faff480c7a
Olena Avdiunina <elena.avdiunina@gmail.com>
2023-01-06 10:13:04 +0200

    added real server link

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,7 +9,7 @@ import icon from "../images/emoji.svg";
 import styles from "../styles/Chat.module.css";
 import Messages from "./Messages";
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://realtime-chat-server-5xq9.onrender.com/");
 
 const Chat = () => {
   const { search } = useLocation();
